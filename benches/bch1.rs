1e356edb80218f06ca3aa9108a268fe38d3172e6
Mauricio Maroto <maulberto3@hotmail.com>
2024-07-07 22:28:47 -0600

    testing benchmark

---
+++ benches/bch1.rs
@@ -0,0 +1,25 @@
+// benches/eigen_benchmark.rs
+
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use ndarray::{array, Array2};
+use ndarray_linalg::Eig;
+
+pub fn eigen_decomposition_benchmark(c: &mut Criterion) {
+    let a: Array2<f64> = array![
+        [1.01, 0.86, 4.60],
+        [3.98, -0.53, 7.04],
+        [3.30, 8.26, 3.89],
+    ];
+
+    c.bench_function("eigen_decomposition", |b| {
+        b.iter(|| {
+            let (eigs, vecs) = a.clone().eig().unwrap();
+            // // Ensure the calculation produces a result that is used
+            // black_box(eigs);
+            // black_box(vecs);
+        });
+    });
+}
+
+criterion_group!(benches, eigen_decomposition_benchmark);
+criterion_main!(benches);
