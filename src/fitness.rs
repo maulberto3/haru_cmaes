4bc5924893daad5bc317f87498f786c83ff1f6da
Mauricio Maroto <maulberto3@hotmail.com>
2024-10-13 12:10:30 -0600

    reference in squaresum

--- src/fitness.rs
+++ src/fitness.rs
@@ -13,6 +13,7 @@ pub trait FitnessEvaluator {
     fn evaluate(&self, pop: &PopulationY) -> Result<Fitness>;
 }
 
+/// Example of a fitness function
 /// Implementation of the square and sum as simple fitness function.
 pub struct SquareAndSum;
 
@@ -20,7 +21,7 @@ impl FitnessEvaluator for SquareAndSum {
     fn evaluate(&self, pop: &PopulationY) -> Result<Fitness> {
         let values = pop
             .y
-            .map_axis(Axis(1), |row| row.mapv(|elem| elem.powi(2)).sum())
+            .map_axis(Axis(1), |row| row.map(|elem| elem.powi(2)).sum())
             // .view()
             .into_shape((pop.y.nrows(), 1))
             .unwrap()
@@ -30,7 +31,8 @@ impl FitnessEvaluator for SquareAndSum {
     }
 }
 
-/// Implementation of the square and sum fitness function.
+/// Example of a fitness function
+/// Another eImplementation of the square and sum fitness function.
 pub struct SimpleStd;
 
 impl FitnessEvaluator for SimpleStd {
