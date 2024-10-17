5d95be7dd2a777b9a672677451c785c04e63b786
Mauricio Maroto <maulberto3@hotmail.com>
2024-10-16 21:06:40 -0600

    removed cwrapper nlopt

--- benches/nlopt.rs
+++ benches/nlopt.rs
@@ -1,16 +1,16 @@
-use criterion::{criterion_group, criterion_main, Criterion};
-include!("../assets/nlopt.rs"); // Include the `simple_nlopt.rs` file
+// use criterion::{criterion_group, criterion_main, Criterion};
+// include!("../assets/nlopt.rs"); // Include the `simple_nlopt.rs` file
 
-/// Benchmark function for the `nlopt` example.
-///
-/// This function measures the performance of the `example` function from the `simple_nlopt` module.
-///
-/// # Arguments
-///
-/// * `c` - A mutable reference to a `Criterion` object for benchmarking.
-fn nlopt_benchmark(c: &mut Criterion) {
-    c.bench_function("NLOpt Simple Example", |b| b.iter(|| example())); // Use the `example` from simple_nlopt.rs
-}
+// /// Benchmark function for the `nlopt` example.
+// ///
+// /// This function measures the performance of the `example` function from the `simple_nlopt` module.
+// ///
+// /// # Arguments
+// ///
+// /// * `c` - A mutable reference to a `Criterion` object for benchmarking.
+// fn nlopt_benchmark(c: &mut Criterion) {
+//     c.bench_function("NLOpt Simple Example", |b| b.iter(|| example())); // Use the `example` from simple_nlopt.rs
+// }
 
-criterion_group!(benches, nlopt_benchmark);
-criterion_main!(benches);
+// criterion_group!(benches, nlopt_benchmark);
+// criterion_main!(benches);
