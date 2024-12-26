31fec383664968f3c8d046115ec091a6509c2096
Mauricio Maroto <maulberto3@hotmail.com>
2024-12-26 13:03:26 -0600

    feat(strategy): fix closeness to target stopping criteria

--- src/simple_use.rs
+++ src/simple_use.rs
@@ -1,10 +1,11 @@
 use std::time::Instant;
 
 use crate::{
-    fitness::{allow_objective_func, FitnessEvaluator, SquareAndSum},
+    fitness::{allow_objective_func, FitnessEvaluator, SquareAndSum, StdAndSum},
     CmaesAlgo, CmaesAlgoOptimizer, CmaesParams, CmaesParamsValidator, CmaesState, CmaesStateLogic,
 };
 use anyhow::Result;
+use ndarray::Array1;
 
 // #[allow(unused_imports)]
 // use blas_src;
@@ -31,14 +32,15 @@ pub fn example() -> Result<()> {
     let start = Instant::now();
 
     // Create cost function, i.e. see fitness.rs for an example
-    let objective_function = SquareAndSum { cost_dim: 45 };
+    let objective_function = SquareAndSum { obj_dim: 40 };
+    // let objective_function = StdAndSum { obj_dim: 45 };
 
     // Check allowed objective function
     let (obj, obj_dim) = allow_objective_func(objective_function)?;
 
     // Initialize CMA-ES parameters
     let params = CmaesParams::new()?
-        .set_popsize(50)?
+        .set_popsize(20)?
         .set_xstart(vec![0.0; obj_dim])?
         .set_sigma(0.75)?;
 
@@ -49,7 +51,7 @@ pub fn example() -> Result<()> {
     let mut state = CmaesState::init_state(&cmaes.params)?;
 
     // Run the CMA-ES algorithm until close to objective value
-    let mut step = 0;
+    let (mut step, mut best_y) = (0, vec![99.]);
     loop {
         // Generate a new population
         let mut pop = cmaes.ask(&mut state)?;
@@ -60,12 +62,25 @@ pub fn example() -> Result<()> {
         // Update the state with the new population and fitness values
         state = cmaes.tell(state, &mut pop, &mut fitness)?;
 
-        // // Are we there yet?
-        let curr = state.best_y.first().unwrap();
-        if (curr - cmaes.params.obj_value).abs() < cmaes.params.tol {
-            // If we are close to obj_value less than tol, we are there (break)
+        // Save current best 
+        best_y.push(*state.best_y_fit.first().unwrap());
+
+        // Stopping criteria 1: Check if we are there yet?
+        let last_50_best_y = if best_y.len() > 50 {
+            best_y[best_y.len() - 50..].to_vec()
+        } else {
+            best_y[..].to_vec()
+        };
+        let best_y_avg = Array1::from_vec(last_50_best_y).mean().unwrap();
+        ////////////////
+        // TODO
+        // Allow flag for verbose state
+        print!("{:+.4?}  ", &best_y_avg);
+        ////////////////
+        if (state.best_y_fit.first().unwrap() - best_y_avg).abs() < cmaes.params.tol {
+            println!("Search stopped due to closeness to target");
             break;
-        }
+        }    
 
         step += 1;
     }
@@ -73,7 +88,7 @@ pub fn example() -> Result<()> {
     println!(
         "Step {} | Fitness: {:+.4?} | Duration p/step: {:.4} secs",
         step,
-        &state.best_y.first().unwrap(),
+        &state.best_y_fit.first().unwrap(),
         (start.elapsed().as_micros() as f32) / 1000000.0 / (step as f32)
     );
 
