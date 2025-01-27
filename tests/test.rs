b6e953f6ca24ecbafe71ce13780cf4234f96e0a2
Mauricio Maroto <maulberto3@hotmail.com>
2025-01-27 00:28:32 -0600

    fix: more clarity in first test

--- tests/test.rs
+++ tests/test.rs
@@ -3,7 +3,16 @@ mod tests {
     use haru_cmaes::ask_tell_use;
 
     #[test]
-    fn end_to_end_test() {
+    fn test_ask_tell_use() {
         assert!(ask_tell_use::ask_tell_example().is_ok());
     }
+
+    // #[test]
+    // fn test_express_use() {
+    //     let obj_func = SquareAndSum {
+    //         obj_dim: 5,
+    //         dir: MinOrMax::Min,
+    //     };
+    //     assert_eq!(express_use::express_executor(obj_func), true);
+    // }
 }
