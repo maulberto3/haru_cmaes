6dab3d3d2aa5a815d845f0ad4fed4c7645507638
Mauricio <maulberto3@hotmail.com>
2026-05-29 16:56:41 -0600

    refactor: update .gitignore and remove objectives module from lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,6 @@
 //! For detailed usage and examples, please refer to `lib.rs` and the examples directory.
 
 pub mod fitness;
-pub mod objectives;
 pub mod params;
 pub mod state;
 pub mod strategy;
