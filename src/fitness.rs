6846c9f5ff1dfd58203d960e08727f8aef184777
Mauricio <maulberto3@hotmail.com>
2026-05-30 22:43:39 -0600

    refactor: enhance documentation for fitness structures and evaluator trait

--- src/fitness.rs
+++ src/fitness.rs
@@ -1,97 +1,77 @@
 use anyhow::Result;
 use nalgebra::{DMatrix, DVector};
 
-/// Struct to hold the population as normal data points
+/// Struct to hold the population as standard normal data points
+///
+/// # Example
+/// ```rust
+/// use haru_cmaes::fitness::PopulationZ;
+/// use nalgebra::{DMatrix, Matrix3x4};
+/// let static_matrix = Matrix3x4::new(
+///     -0.1, 0.2, 0.3, -0.3,
+///     0.4, -0.2, -0.6, 0.6,
+///     0.7, -0.8, 0.9, -0.9,
+/// );
+/// let z = DMatrix::from_row_slice(3, 4, static_matrix.as_slice());
+/// let pop_z = PopulationZ { z };
+/// assert!(pop_z.z.shape() == (3, 4));
+/// ```
 #[derive(Debug, Clone)]
 pub struct PopulationZ {
     pub z: DMatrix<f32>,
 }
 
-/// Struct to hold the population as (eigen-)rotated data points
-#[derive(Debug, Clone)]
-pub struct PopulationY {
-    pub y: DMatrix<f32>,
-}
-
-#[derive(Debug, Clone)]
-pub enum MinOrMax {
-    Min,
-    Max,
-}
-
-/// Structure to hold fitness values of a population.
-#[derive(Debug, Clone)]
-pub struct Fitness {
-    pub values: DVector<f32>,
-}
-
-/// A trait for fitness evaluation.
-///
-/// Implements the full evaluate/evaluator_dim pipeline for any objective function.
-pub trait FitnessEvaluator {
-    fn evaluate(&self, pop: &PopulationY) -> Result<Fitness>;
-    fn evaluator_dim(&self) -> Result<usize>;
-}
-
-/// Generic fitness wrapper for user-defined closures.
-///
-/// `UserFitness` allows users to define custom objective functions as simple closures
-/// that take a single individual's vector and return a scalar fitness value.
-/// The trait implementation automatically handles mapping over entire populations
-/// and applying the optimization direction (min/max).
+/// Struct to hold the population as eigen rotated and scaled data points
 ///
 /// # Example
-///
 /// ```rust
-/// use haru_cmaes::fitness::{PopulationY, FitnessEvaluator, UserFitness, MinOrMax};
-/// use nalgebra::{Matrix3x4, DMatrix};
-///
+/// use haru_cmaes::fitness::PopulationY;
+/// use nalgebra::{DMatrix, Matrix3x4};
 /// let static_matrix = Matrix3x4::new(
 ///     1.0, 2.0, 3.0, 3.5,
 ///     4.0, 5.0, 6.0, 6.5,
 ///     7.0, 8.0, 9.0, 9.5,
 /// );
 /// let y = DMatrix::from_row_slice(3, 4, static_matrix.as_slice());
-/// let pop = PopulationY { y };
+/// let pop_y = PopulationY { y };
+/// assert!(pop_y.y.shape() == (3, 4));
+/// ```
+#[derive(Debug, Clone)]
+pub struct PopulationY {
+    pub y: DMatrix<f32>,
+}
+
+/// Enum to specify optimization direction (minimization or maximization)
+#[derive(Debug, Clone, PartialEq)]
+pub enum MinOrMax {
+    Min,
+    Max,
+}
+
+/// Structure to hold fitness values of a population
 ///
-/// // Define a custom objective with a closure (sum of squares)
-/// let objective_function = UserFitness::new(
-///     |individual: &nalgebra::DVector<f32>| {
-///         individual.iter().map(|x| x.powi(2)).sum()
-///     },
-///     4,
-///     MinOrMax::Min,
-/// );
-/// let fitness = objective_function.evaluate(&pop).unwrap();
-///
-/// // We should have one fitness value per individual
+/// # Example
+/// ```rust
+/// use haru_cmaes::fitness::Fitness;
+/// use nalgebra::DVector;
+/// let fitness_values = DVector::from_vec(vec![0.1, 0.2, 0.3]);
+/// let fitness = Fitness { values: fitness_values };
 /// assert!(fitness.values.shape() == (3, 1));
 /// ```
-pub struct UserFitness<F>
-where
-    F: Fn(&DVector<f32>) -> f32,
-{
-    func: F,
-    obj_dim: usize,
-    dir: MinOrMax,
+#[derive(Debug, Clone)]
+pub struct Fitness {
+    pub values: DVector<f32>,
 }
 
-impl<F> UserFitness<F>
-where
-    F: Fn(&DVector<f32>) -> f32,
-{
-    /// Create a new `UserFitness` objective function.
-    ///
-    /// # Arguments
-    ///
-    /// * `func` - A closure that takes an individual (DVector) and returns a fitness value (f32)
-    /// * `obj_dim` - The dimension of the objective space
-    /// * `dir` - Optimization direction (`MinOrMax::Min` or `MinOrMax::Max`)
-    pub fn new(func: F, obj_dim: usize, dir: MinOrMax) -> Self {
-        UserFitness { func, obj_dim, dir }
-    }
+/// A trait for fitness evaluation
+///
+/// Implements the full evaluate and evaluator_dim methods for any user-defined objective function
+pub trait FitnessEvaluator {
+    fn evaluate(&self, pop: &PopulationY) -> Result<Fitness>;
+    fn evaluator_dim(&self) -> Result<usize>;
 }
 
+/// Implement the `FitnessEvaluator` trait for `UserFitness` to allow it to be used as an objective function in the CMA-ES algorithm.
 impl<F> FitnessEvaluator for UserFitness<F>
 where
     F: Fn(&DVector<f32>) -> f32,
@@ -117,5 +97,69 @@ where
 
     fn evaluator_dim(&self) -> Result<usize> {
         Ok(self.obj_dim)
+    }
+}
+
+/// Generic fitness wrapper for user-defined objective function.
+///
+/// `UserFitness` allows users to define custom objective functions as simple closures
+/// that take a single individual's vector and return a scalar fitness value.
+///
+/// The trait implementation automatically handles mapping over entire populations
+/// and applying the optimization direction (min/max).
+///
+/// NOTE: Be sure to match the `obj_dim` with the dimension of the individuals in the population for correct evaluation.
+///
+/// # Example
+///
+/// ```rust
+/// use haru_cmaes::fitness::{PopulationY, FitnessEvaluator, UserFitness, MinOrMax};
+/// use nalgebra::{Matrix3x4, DMatrix};
+///
+/// let static_matrix = Matrix3x4::new(
+///     1.0, 2.0, 3.0, 3.5,
+///     4.0, 5.0, 6.0, 6.5,
+///     7.0, 8.0, 9.0, 9.5,
+/// );
+/// let y = DMatrix::from_row_slice(3, 4, static_matrix.as_slice());
+/// let pop = PopulationY { y };
+///
+/// // Define a custom objective with a closure (sum of squares)
+/// let objective_function = UserFitness::new(
+///     |individual: &nalgebra::DVector<f32>| {
+///         let mut sum = 0.0;
+///         for i in 0..individual.len() {
+///             sum += individual[i].powi(2);
+///         }
+///         sum
+///     },
+///     4, // This needs to match the CMAES algorith initial parameter dimension.
+///     MinOrMax::Min,
+/// );
+/// assert_eq!(objective_function.evaluator_dim().unwrap(), 4);
+/// assert_eq!(objective_function.dir, MinOrMax::Min);
+/// ```
+pub struct UserFitness<F>
+where
+    F: Fn(&DVector<f32>) -> f32,
+{
+    pub func: F,
+    pub obj_dim: usize,
+    pub dir: MinOrMax,
+}
+
+impl<F> UserFitness<F>
+where
+    F: Fn(&DVector<f32>) -> f32,
+{
+    /// Create a new `UserFitness` objective function.
+    ///
+    /// # Arguments
+    ///
+    /// * `func` - A closure that takes an individual (DVector) and returns a fitness value (f32)
+    /// * `obj_dim` - The dimension of the objective space
+    /// * `dir` - Optimization direction (`MinOrMax::Min` or `MinOrMax::Max`)
+    pub fn new(func: F, obj_dim: usize, dir: MinOrMax) -> Self {
+        UserFitness { func, obj_dim, dir }
     }
 }
